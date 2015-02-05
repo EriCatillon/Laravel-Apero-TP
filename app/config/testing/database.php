959369c0d4b1913aed10a776d79356082597fdf4
EriCatillon <ericatillon@gmail.com>
2015-02-05 11:10:20 +0100

    'add'

--- app/config/testing/database.php
+++ app/config/testing/database.php
@@ -12,10 +12,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost',
-			'database'  => 'apero_testing',
-			'username'  => 'root',
-			'password'  => '',
+			'host'      => getenv('DB_HOST'),
+			'database'  => getenv('DB_NAME'),
+			'username'  => getenv('DB_USERNAME'),
+			'password'  => getenv('DB_PASSWORD'),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
